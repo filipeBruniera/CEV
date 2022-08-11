891982a4e81a2ed1337d25e0195659b3982989e2
filipeBruniera <filipebruniera@gmail.com>
2022-08-11 13:48:51 -0300

    inicio

---
+++ Aulas/a7-8.js
@@ -0,0 +1,16 @@
+let n = 10;
+console.log((n += 5));
+
+let a1 = 5 > 2;
+let a2 = 7 < 4;
+let a3 = 8 >= 8;
+let a4 = 9 <= 7;
+let a5 = 5 == 5;
+
+// = atribuição == valores (5 é igual a '5') === valores e tipos (5 != de '5')
+
+console.log(`5 é maior que 2? ${a1}\n
+7 é menor que 4? ${a2}\n
+8 é maior ou igual a 8? ${a3}\n
+9 é menor ou iguala 7 ${a4}\n
+5 é igual a 5? ${a5}`);
